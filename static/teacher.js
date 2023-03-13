7b4f3b3ce78b2085eacb545e45268c3609419774
jaimanm <93413974+jaimanm@users.noreply.github.com>
2023-03-13 16:26:04 +0000

    added search functionality in table

--- static/teacher.js
+++ static/teacher.js
@@ -1,30 +1,29 @@
-$.get("data.csv", function(data) {
-    // parse the data from data.csv into the html table
-    var htmlTable = document.getElementById("table");
-    var dataRows = data.split("\n");
-    // console.log(dataRows);
-    var initialRows = htmlTable.rows.length;
-    for (var i = 0; i < dataRows.length; i++) {
-        var newRow = htmlTable.insertRow(i + initialRows);
-        var insertRow = dataRows[i].split(",");
-        for (var j = 0; j < insertRow.length; j++) {
-            var newCell = newRow.insertCell(j);
-            newCell.innerHTML = insertRow[j];
-            if (j == 2) newCell.contentEditable = true;
-        }
-        var buttons = newRow.insertCell(3);
-        buttons.innerHTML = "<button id='add' contenteditable='false' onclick='btnAction()''>Add</button><button id='subtract' contenteditable='false' onclick='btnAction()''>Subtract</button>";
-    }
-});
+// // from a tutorial (https://youtu.be/qp7PdguA0Vg)
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".search-input").forEach((inputField) => {
+    const tableRows = inputField
+      .closest("table")
+      .querySelectorAll("tbody > tr");
+    const headerCell = inputField.closest("th");
+    const otherHeaderCells = headerCell.closest("tr").children;
+    const columnIndex = Array.from(otherHeaderCells).indexOf(headerCell);
+    const searchableCells = Array.from(tableRows).map(
+      (row) => row.querySelectorAll("td")[columnIndex]
+    );
 
-function btnAction() {
-    var button = event.target;
-    var row = button.parentNode.parentNode;
-    var cell = row.cells[2];
-    var value = parseInt(cell.innerHTML);
-    if (button.id == "add") {
-        cell.innerHTML = value + 1;
-    } else if (button.id == "subtract") {
-        cell.innerHTML = value - 1;
-    }
-}
+    inputField.addEventListener("input", () => {
+      const searchQuery = inputField.value.toLowerCase();
+
+      for (const tableCell of searchableCells) {
+        const row = tableCell.closest("tr");
+        const value = tableCell.textContent.toLowerCase().replace(",", "");
+
+        row.style.visibility = null;
+
+        if (value.search(searchQuery) === -1) {
+          row.style.visibility = "collapse";
+        }
+      }
+    });
+  });
+});
